b16a52105977357c1ec05575ff65c5353b48c20c
Chiarra <103839751+chiarraeu@users.noreply.github.com>
2024-02-05 10:31:38 +0200

    Update CookiesServiceProvider.php

--- app/Providers/CookiesServiceProvider.php
+++ app/Providers/CookiesServiceProvider.php
@@ -19,9 +19,10 @@ class CookiesServiceProvider extends ServiceProvider
             ->csrf();
 
         /* Register all Analytics cookies at once using one single shorthand method:
+*/        
         Cookies::analytics()
-            ->google(env('GOOGLE_ANALYTICS_ID'));
-        */
+           ->google(env('GOOGLE_ANALYTICS_ID'));
+        
         // Register custom cookies under the pre-existing "optional" category:
         Cookies::optional()
             ->name('darkmode_enabled')
